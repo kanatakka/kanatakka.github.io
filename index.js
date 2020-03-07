6b27adae12d1db41017b25ff3fb864f181cce4a6
kanatakka <takashi.kngw@gmail.com>
2020-03-07 20:19:43 +0900

    aaa

--- index.js
+++
@@ -1,8 +0,0 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    seen: true
-  }
-})
-
-app.seen = true;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      }
+    ]
+  },
+  // エントリポイントのファイル
+  entry: "./src/main.js",
+  output: {
+    // 出力先のディレクトリ
+    path: path.resolve(__dirname, "./public"),
+    // 出力ファイル名
+    filename: "bundle.js"
+  },
+  plugins: [new VueLoaderPlugin()],
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      // vueのディレクトリ指定っぽい？
+      'vue$': 'vue/dist/vue.js'
+    }
+  },
+  devServer: {
+    // webpackの扱わないファイル(HTMLや画像など)が入っているディレクトリ
+    contentBase: path.resolve(__dirname, "public")
+  }
+};
